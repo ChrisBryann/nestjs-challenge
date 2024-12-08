f2dac8b583571aee83cf0d8cf410b8ccf87fd11b
Christopher Bryan <christopher_bryan@rocketmail.com>
2024-12-08 09:26:19 +0000

    trying out Jwt strategy with Auth0

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UsersService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: './env' }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, UsersService],
+  exports: [PassportModule],
 })
 export class AuthModule {}
