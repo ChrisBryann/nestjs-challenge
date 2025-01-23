31f48585309dc9c8afe7abea8ee558960c182cba
Christopher Bryan <christopher_bryan@rocketmail.com>
2025-01-23 00:29:44 +0000

    added jwt auth guard to project controller

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, UseGuards } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-refresh-auth.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('projects')
 @UseGuards(JwtAuthGuard)
