76bc3b8bf74fb172c9da29ca7d8c1c976aed2b97
Christopher Bryan <christopher_bryan@rocketmail.com>
2025-01-24 18:42:36 +0000

    reconfigured ports for auth and projects and added pgadmin4 service in docker

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,21 +1,24 @@
-import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [TypeOrmModule.forRootAsync({
-        useFactory: (configService: ConfigService) => ({
-          
-          type: 'postgres',
-          host: configService.getOrThrow<string>('POSTGRES_HOST'),
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('POSTGRES_HOST'),
         port: +configService.getOrThrow<string>('POSTGRES_PORT'),
         username: configService.getOrThrow<string>('POSTGRES_USERNAME'),
         password: configService.getOrThrow<string>('POSTGRES_PASSWORD'),
         database: configService.getOrThrow<string>('POSTGRES_DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts, .js}'],
+        autoLoadEntities: true,
         synchronize: true, // Set to false in production
-        }),
-        inject: [ConfigService]
-      }),]
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
